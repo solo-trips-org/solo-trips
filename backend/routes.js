9c0856169b00dd83f38871a063c7d11c95e670d3
P.Thamilselven <contact@tselven.com>
2025-09-14 21:31:48 +0530

    develop fetch ratings, create rating methods and endpoints

--- backend/routes.js
+++ backend/routes.js
@@ -6,8 +6,7 @@ import {
   logout,
   sendOTP,
   newOTP,
-  forgetPassword,
-  resetPassword,
+  resetPasswordWithOTP,
 } from "./app/controllers/auth.controller.js";
 import { requireAuth } from "./app/middlewares/auth.middleware.js";
 import User from "./app/models/user.model.js";
@@ -24,28 +23,56 @@ import { getNearbyGuides } from "./app/controllers/guide.controller.js";
 import { checkOTP } from "./app/middlewares/checkOTP.middleware.js";
 import { getDashboardAnalytics } from "./app/controllers/dashboard.controller.js";
 import { globalSearch } from "./app/controllers/search.controller.js";
+import {
+  addEventRating,
+  addGuideRating,
+  addHotelRating,
+  addPlaceRating,
+  getEventRatings,
+  getGuideRatings,
+  getHotelRatings,
+  getPlaceRatings,
+} from "./app/controllers/rating.controller.js";
 
 const router = Router();
 
+//----------------- Auth Routes --------------------//
 router.post("/register", checkOTP, register);
 router.post("/login", login);
 router.get("/profile", requireAuth, profile);
 router.post("/logout", requireAuth, logout);
 router.post("/send-otp", sendOTP);
 router.post("/new-otp", newOTP);
-router.post("/forget-password", forgetPassword);
-router.post("/reset-password", resetPassword);
+router.post("/forget-password", resetPasswordWithOTP);
 
 router.use("/places", requireAuth, allowedRoles("admin"), createCrud(Place));
 router.use("/events", requireAuth, allowedRoles("admin"), createCrud(Event));
 router.use("/guides", requireAuth, allowedRoles("admin"), createCrud(Guide));
 router.use("/users", requireAuth, allowedRoles("admin"), createCrud(User));
 
+//----------------- NearBy Finding Routes ---------------------//
 router.get("/near/places", requireAuth, getNearbyPlaces);
 router.get("/near/hotels", requireAuth, getNearbyHotels);
 router.get("/near/events", requireAuth, getNearbyEvents);
 router.get("/near/guides", requireAuth, getNearbyGuides);
-router.get("/analytics", requireAuth , allowedRoles("admin") , getDashboardAnalytics);
+
+
+router.get(
+  "/analytics",
+  requireAuth,
+  allowedRoles("admin"),
+  getDashboardAnalytics
+);
 router.get("/search", globalSearch);
+
+//-------------------------- Ratings Routes ------------------------//
+router.post("/ratings/place", addPlaceRating);
+router.post("/ratings/hotel", addHotelRating);
+router.post("/ratings/event", addEventRating);
+router.post("/ratings/guide", addGuideRating);
+router.get("/ratings/place/:placeId", getPlaceRatings);
+router.get("/ratings/hotel/:hotelId", getHotelRatings);
+router.get("/ratings/event/:eventId", getEventRatings);
+router.get("/ratings/guide/:guideId", getGuideRatings);
 
 export default router;
